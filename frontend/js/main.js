cb6ee686801a7ada66b7af25d583fd0f7241f894
pistirozsa <pisti.rzsa@gmail.com>
2023-10-25 17:07:04 +0200

    basic sort by feature

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -5,7 +5,7 @@ fetch("/data/data.json")
     return response.json();
   })
   .then((data) => {
-    console.log("Full data:",data); // This will log the JSON data to the console
+    console.log("Full data:", data); // This will log the JSON data to the console
     // Group by 'score' and count the records
     const groupedDataByScore = data.reduce((acc, obj) => {
       const key = obj.Score;
@@ -18,48 +18,89 @@ fetch("/data/data.json")
 
     // Convert the grouped data back to an array of objects
     const groupByScoreResult = Object.values(groupedDataByScore);
-    drawHorizontalBarChart("#score-chart", groupByScoreResult, "Score", "Count");
+    drawHorizontalBarChart(
+      "#score-chart",
+      groupByScoreResult,
+      "Score",
+      "Count"
+    );
 
     // Output the result
-    console.log("Group by score:",groupByScoreResult);
+    console.log("Group by score:", groupByScoreResult);
 
     // Group by 'score' and count the records
     const groupedDataByRobertaLabel = data.reduce((acc, obj) => {
-        const key = obj.roberta_label;
-        if (!acc[key]) {
-          acc[key] = { roberta_label: key, Count: 0 };
-        }
-        acc[key].Count++;
-        return acc;
-      }, {});
-  
-      // Convert the grouped data back to an array of objects
-      const groupByScoreRobertaLabel = Object.values(groupedDataByRobertaLabel);
-      drawHorizontalBarChart("#roberta-chart", groupByScoreRobertaLabel, "roberta_label", "Count");
+      const key = obj.roberta_label;
+      if (!acc[key]) {
+        acc[key] = { roberta_label: key, Count: 0 };
+      }
+      acc[key].Count++;
+      return acc;
+    }, {});
 
-      // Output the result
-      console.log("Group by roberta label:",groupByScoreRobertaLabel);
+    // Convert the grouped data back to an array of objects
+    const groupByScoreRobertaLabel = Object.values(groupedDataByRobertaLabel);
+    drawHorizontalBarChart(
+      "#roberta-chart",
+      groupByScoreRobertaLabel,
+      "roberta_label",
+      "Count"
+    );
 
-      // Group by 'score' and count the records
+    // Output the result
+    console.log("Group by roberta label:", groupByScoreRobertaLabel);
+
+    // Group by 'score' and count the records
     const groupedDataByVaderLabel = data.reduce((acc, obj) => {
-        const key = obj.vader_label;
-        if (!acc[key]) {
-          acc[key] = { vader_label: key, Count: 0 };
-        }
-        acc[key].Count++;
-        return acc;
-      }, {});
-  
-      // Convert the grouped data back to an array of objects
-      const groupByScoreVaderLabel = Object.values(groupedDataByVaderLabel);
-      drawHorizontalBarChart("#vader-chart", groupByScoreVaderLabel, "vader_label", "Count");
-  
-      // Output the result
-      console.log("Group by vader label:",groupByScoreVaderLabel);
+      const key = obj.vader_label;
+      if (!acc[key]) {
+        acc[key] = { vader_label: key, Count: 0 };
+      }
+      acc[key].Count++;
+      return acc;
+    }, {});
 
+    // Convert the grouped data back to an array of objects
+    const groupByScoreVaderLabel = Object.values(groupedDataByVaderLabel);
+    drawHorizontalBarChart(
+      "#vader-chart",
+      groupByScoreVaderLabel,
+      "vader_label",
+      "Count"
+    );
+
+    // Output the result
+    console.log("Group by vader label:", groupByScoreVaderLabel);
+
+    let comboboxSortCriteria = d3.select("#sortCriteria");
+    comboboxSortCriteria.on("change", function () {
+      // Get the selected value
+      let selectedCriteria = d3.select(this).property("value");
+      console.log("Selcted Sort Criteria: ", selectedCriteria);
+      data.sort(function(a, b) {
+        if (a[selectedCriteria] < b[selectedCriteria]) {
+            return -1;
+        }
+        if (a[selectedCriteria] > b[selectedCriteria]) {
+            return 1;
+        }
+        return 0;
+      });
+      console.log(data);
+      var element = document.getElementById("post-list");
+      element.innerHTML = "";
       for (let i = 0; i < 15; i++) {
-        document.getElementById('post-list').innerHTML += generateFeedbackHTML(data[i]);
-      }    
+        document.getElementById("post-list").innerHTML += generateFeedbackHTML(
+          data[i]
+        );
+      }
+    });
+
+    for (let i = 0; i < 15; i++) {
+      document.getElementById("post-list").innerHTML += generateFeedbackHTML(
+        data[i]
+      );
+    }
   })
   .catch((error) => {
     console.error("Error:", error);
